c3f51c58ef84df1aaa840d1028ec558fb81d4b08
Inshaal Tahir <inshaaltahir2@gmail.com>
2020-10-25 22:21:56 +0500

    My first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var parmA = req.param('a');
+  var parmB = req.param('b');
+
+  let a = parmA + parmB;
+
+  res.render('index', { title: a });
+});
+
+module.exports = router;
